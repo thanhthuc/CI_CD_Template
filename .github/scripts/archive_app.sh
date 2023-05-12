6bd87a4a60184e938b27cd97185c73d919f26da4
Thanh Thuc <thanhthuc.ng@samsung.com>
2023-05-12 18:23:20 +0700

    Fix syntax error

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -3,9 +3,8 @@
 set -euo pipefail
 
 xcodebuild -project ExampleApp/ExampleApp.xcodeproj \
-            -scheme ExampleApp
+            -scheme ExampleApp \
+            -sdk iphoneos \
             -configuration AppStoreDistribution \
             -archivePath $PWD/build/ExampleApp.xcarchive \
             clean archive | xcpretty
-
-#            -sdk iphoneos \
