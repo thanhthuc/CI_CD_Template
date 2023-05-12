0a7aa2da4c7391fe622cae82c13d9c49fc7c8f13
Thanh Thuc <thanhthuc.ng@samsung.com>
2023-05-12 10:01:34 +0700

    Add new path

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 #SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-SCHEME="$(xcodebuild -project ExampleApp/ExampleApp.xcodeproj -list -json | jq -r '.project.schemes[0]')"
+SCHEME="~/ExampleApp/$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
