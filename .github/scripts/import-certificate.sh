89ce1250d0c1e9842c40ec1889e9dbc30e74d07a
Nguyen Thanh Thuc <thanhthucbkinfo@yahoo.com.vn>
2023-05-14 06:51:32 +0700

    Edit certificate script

--- .github/scripts/import-certificate.sh
+++ .github/scripts/import-certificate.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-security create-keychain -p "" build.keychain
-security list-keychains -s build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p "" build.keychain
+security create-keychain -p "" login.keychain
+security list-keychains -s login.keychain
+security default-keychain -s login.keychain
+security unlock-keychain -p "" login.keychain
 security set-keychain-settings
 security import <(echo $SIGNING_CERTIFICATE_P12_DATA | base64 --decode) \
                 -f pkcs12 \
-                -k build.keychain \
+                -k login.keychain \
                 -P $SIGNING_CERTIFICATE_PASSWORD \
                 -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple: -s -k "" build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" login.keychain
